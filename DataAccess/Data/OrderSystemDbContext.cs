8a10389460df8561b948652db7b9685b6d0ea744
yacnuzun <yacn.uzun@gmail.com>
2022-09-14 13:31:17 +0300

    Test and edit complited. will start readme.

--- DataAccess/Data/OrderSystemDbContext.cs
+++ DataAccess/Data/OrderSystemDbContext.cs
@@ -22,9 +22,29 @@ namespace DataAccess.Concrete.DatabaseContext
         }
 
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+               new Product { Id = 123, Name = "Elma", ProductCode = "3007975", Amount = 20 },
+               new Product { Id = 124, Name = "Armut", ProductCode = "11", Amount = 40 },
+               new Product { Id = 1234, Name = "Ayva", ProductCode = "w-l-12532w2i", Amount = 30 },
+               new Product { Id = 1235, Name = "Maydonoz", ProductCode = "065781-31966", Amount = 12 },
+               new Product { Id = 1236, Name = "Havuç", ProductCode = "12321321", Amount = 5 }
+               );
+            modelBuilder.Entity<Order>().HasData(
+               new Order { Id = 123, CompanyId = 1, TotalAmount = 40, Date = DateTime.Parse("11-02-2022") },
+               new Order { Id = 124, CompanyId = 1, TotalAmount = 100, Date = DateTime.Parse("11-02-2022") }
+               );
+            modelBuilder.Entity<OrderProduct>().HasData(
+               new OrderProduct { Id = 123, OrderId = 123, ProductId = 123 },
+               new OrderProduct { Id = 124, OrderId = 123, ProductId = 123 },
+               new OrderProduct { Id = 1234, OrderId = 124, ProductId = 1234 },
+               new OrderProduct { Id = 1235, OrderId = 124, ProductId = 1234 },
+               new OrderProduct { Id = 1236, OrderId = 124, ProductId = 124 }
+               );
+        }
 
-        
 
-        
+
     }
 }
---
+++ DataAccess/Migrations/20220914085308_testMigration.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    public partial class testMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "OrderProducts",
+                columns: new[] { "Id", "OrderId", "ProductId" },
+                values: new object[,]
+                {
+                    { 123, 123, 123 },
+                    { 124, 123, 123 },
+                    { 1234, 124, 1234 },
+                    { 1235, 124, 1234 },
+                    { 1236, 124, 124 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Orders",
+                columns: new[] { "Id", "CompanyId", "Date", "TotalAmount" },
+                values: new object[,]
+                {
+                    { 123, 1, new DateTime(2022, 2, 11, 0, 0, 0, 0, DateTimeKind.Unspecified), 40 },
+                    { 124, 1, new DateTime(2022, 2, 11, 0, 0, 0, 0, DateTimeKind.Unspecified), 100 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Amount", "Name", "ProductCode" },
+                values: new object[,]
+                {
+                    { 123, 20, "Elma", "3007975" },
+                    { 124, 40, "Armut", "11" },
+                    { 1234, 30, "Ayva", "w-l-12532w2i" },
+                    { 1235, 12, "Maydonoz", "065781-31966" },
+                    { 1236, 5, "Havuç", "12321321" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "OrderProducts",
+                keyColumn: "Id",
+                keyValue: 123);
+
+            migrationBuilder.DeleteData(
+                table: "OrderProducts",
+                keyColumn: "Id",
+                keyValue: 124);
+
+            migrationBuilder.DeleteData(
+                table: "OrderProducts",
+                keyColumn: "Id",
+                keyValue: 1234);
+
+            migrationBuilder.DeleteData(
+                table: "OrderProducts",
+                keyColumn: "Id",
+                keyValue: 1235);
+
+            migrationBuilder.DeleteData(
+                table: "OrderProducts",
+                keyColumn: "Id",
+                keyValue: 1236);
+
+            migrationBuilder.DeleteData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 123);
+
+            migrationBuilder.DeleteData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 124);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 123);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 124);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1234);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1235);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1236);
+        }
+    }
+}
